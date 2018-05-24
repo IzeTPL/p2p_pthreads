94b877de47791fbbccd0ea45eb36fbda5e8901d6
Marian <IzeTPL@bitbucket.org>
2018-05-24 12:59:41 +0200

    prawie działa

--- p2p.c
+++ p2p.c
@@ -9,14 +9,13 @@
 #include <strings.h>
 #include <netdb.h>
 
-#define PORT 2137
-#define ADDRESS "127.0.0.1"
+#define PORT 8080
 //#define ADDRESS_MASK "172.16.1.%d"
 #define ADDRESS_MASK "127.0.0.%d"
 
 #define POEM_LINE "BLABLA"
-#define POEM_LINE_CLIENT 12
-#define POEM_LINE_NUM 12
+#define POEM_LINE_CLIENT 1
+#define POEM_LINE_NUM 1
 #define POEM_LINE_LENGHT 6
 
 int32_t numbers[POEM_LINE_NUM];
@@ -66,17 +65,18 @@ void *client(void *arg) {
     //Działaj dopóki nie otrzymamy całego wiersza
     while(is_poem_ready() < 0) {
 
-        printf("Poem not ready scanning...\n");
+        //printf("Poem not ready scanning...\n");
 
         client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
         if (client_socket < 0) printf("Client ERROR opening socket\n");
         else {
 
-            printf("Client opening socket\n");
+            //printf("Client opening socket\n");
             server_address.sin_family = AF_INET;
             server_address.sin_port = htons(PORT);
-            printf("Building ip address\n");
+            //printf("Building ip address\n");
+            printf("Attempting to connect to *%d\n", current_ip);
 
             snprintf(ip, sizeof(ip), ADDRESS_MASK, current_ip);
 
@@ -84,7 +84,7 @@ void *client(void *arg) {
                 printf("ERROR bulding ip address\n");
             } else {
 
-                printf("Ip address ready\n");
+                //printf("Ip address ready\n");
 
             }
 
@@ -94,15 +94,17 @@ void *client(void *arg) {
             status = read(client_socket, &poem_line_number, sizeof(int32_t));
             printf("Client got line number %d, from %s\n", poem_line_number, ip);
 
-            poem_line_number--;
+            printf("DEBUG poem line number %d, numbers[%d], %d\n", poem_line_number, poem_line_number - 1 ,numbers[poem_line_number - 1]);
 
-            if (numbers[poem_line_number] == poem_line_number) {
+            if (numbers[poem_line_number - 1] == poem_line_number) {
 
+                //printf("sending no\n");
                 status = write(client_socket, no, 256);
                 printf("Client sent message %s\n", no);
 
-            } else if (poem_line_number > 0 && poem_line_number < 12) {
+            } else if (poem_line_number > 0 && poem_line_number - 1 < POEM_LINE_NUM) {
 
+                //printf("sending yes");
                 status = write(client_socket, yes, 256);
                 printf("Client sent message %s\n", yes);
                 status = read(client_socket, &poem_line_length, sizeof(int32_t));
@@ -110,14 +112,14 @@ void *client(void *arg) {
                 status = read(client_socket, poem_line, poem_line_length);
                 printf("%s\n", poem_line);
 
-                numbers[poem_line_number] = poem_line_number;
+                numbers[poem_line_number - 1] = poem_line_number;
                 *poem[poem_line_number] = poem_line;
 
             }
 
             //current_ip++;
             if (current_ip >= 254)
-                current_ip = 0;
+                current_ip = 1;
 
             close(client_socket);
 
@@ -142,7 +144,7 @@ void *server(void *arg) {
     struct sockaddr_in client_address;
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     int connection_socket;
-    int32_t poem_line_number = POEM_LINE_NUM;
+    int32_t poem_line_number = POEM_LINE_CLIENT;
     int32_t poem_line_length = POEM_LINE_LENGHT;
     int status;
 
@@ -157,7 +159,7 @@ void *server(void *arg) {
     //działaj dopóki nie zakończymy ręcznie
     while(stop < 0) {
 
-        printf("Server waiting for connection\n");
+        //printf("Server waiting for connection\n");
         connection_socket = accept(server_socket, (struct sockaddr *) &client_address, &address_length);
 
         if (connection_socket < 0) printf("Server ERROR on accept\n");
@@ -169,7 +171,7 @@ void *server(void *arg) {
         status = write(connection_socket, &poem_line_number, sizeof(int32_t) );
         if (status < 0) printf("Server number sent %d\n", poem_line_number);
 
-        char buff[3];
+        char buff[256];
         status = read(connection_socket, buff, 256);
         printf("Server got message %s\n", buff);
 
